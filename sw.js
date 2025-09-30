6f30bf8b9313712ad049522e88c635ae22ccf8a9
solbena78380 <solbena87@gmail.com>
2025-09-30 21:24:47 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,69 @@
-const CACHE_NAME = 'jnk-po-v1.0';
+const CACHE_NAME = 'jnk-po-v1.1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  'https://script.google.com/macros/s/AKfycbyY5KonAR-GXFnkHWGEhWD7Hg-Eh2bLSJr2o4iwmniS_mazfWttdAFBe_RdhHiKQcCUQg/exec'
 ];
 
-// Installation
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        return self.skipWaiting();
       })
   );
 });
 
-// Activation
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating...');
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
+    }).then(() => {
+      return self.clients.claim();
     })
   );
 });
 
-// Fetch
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
+  // Ne pas mettre en cache les requêtes vers Google Apps Script
+  if (event.request.url.includes('script.google.com')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+  
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Retourne la ressource en cache ou fetch depuis le réseau
-        return response || fetch(event.request);
-      }
-    )
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).then((response) => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+            
+          return response;
+        });
+      })
   );
 });
