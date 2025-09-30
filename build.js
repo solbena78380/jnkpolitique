4dcdb77ec6e7464ea7bfc613523f66e6e375ffc0
solbena78380 <solbena87@gmail.com>
2025-09-30 23:22:16 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,20 +1,4 @@
 import fs from 'fs-extra';
 
-// Créer le dossier dist
-await fs.ensureDir('dist');
-
-// Copier le HTML principal
-await fs.copy('src/index.html', 'dist/index.html');
-
-// Copier le Service Worker
-await fs.copy('src/sw.js', 'dist/sw.js');
-
-// Copier les icônes si elles existent
-if (await fs.pathExists('src/icons')) {
-  await fs.copy('src/icons', 'dist/icons');
-}
-
-// Copier le manifest
-await fs.copy('src/manifest.json', 'dist/manifest.json');
-
-console.log('Build terminé! Fichiers copiés:', await fs.readdir('dist'));
+// Copie tous les fichiers de src vers dist
+await fs.copy('src', 'dist');
