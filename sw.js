7f153d394cae105b18f23ca7a4b6c0af9aa9d610
solbena78380 <solbena87@gmail.com>
2025-09-30 21:37:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,69 +1,17 @@
-const CACHE_NAME = 'jnk-po-v1.1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://script.google.com/macros/s/AKfycbyY5KonAR-GXFnkHWGEhWD7Hg-Eh2bLSJr2o4iwmniS_mazfWttdAFBe_RdhHiKQcCUQg/exec'
-];
+// Service Worker basique mais fonctionnel
+const CACHE_NAME = 'jnk-po-cache-v1';
 
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => {
-        return self.skipWaiting();
-      })
-  );
+  self.skipWaiting();
+  console.log('Service Worker installé');
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating...');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => {
-      return self.clients.claim();
-    })
-  );
+  event.waitUntil(self.clients.claim());
+  console.log('Service Worker activé');
 });
 
 self.addEventListener('fetch', (event) => {
-  // Ne pas mettre en cache les requêtes vers Google Apps Script
-  if (event.request.url.includes('script.google.com')) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
-  
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        
-        return fetch(event.request).then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-            
-          return response;
-        });
-      })
-  );
+  // Laisser passer toutes les requêtes réseau
+  event.respondWith(fetch(event.request));
 });
